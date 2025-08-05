e3e5f29bb1a4aaf87b14c2104b4f288070444325
Yashwanth Kumar S N <yash004sn@gmail.com>
2025-08-05 10:20:42 +0530

    Fix: Remove basePath and assetPrefix for Vercel

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+// /** @type {import('next').NextConfig} */
+// const isProd = process.env.NODE_ENV === 'production';
 
+// const nextConfig = {
+//   output: 'export',
+//   basePath: isProd ? '/portfolio' : '',
+//   assetPrefix: isProd ? '/portfolio/' : '', // ✅ add trailing slash
+//   images: {
+//     unoptimized: true,
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   compress: true,
+//   optimizeFonts: true,
+// };
+
+// module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio/' : '', // ✅ add trailing slash
+  output: 'export', // Only if you're doing `next export`, else remove this line
   images: {
     unoptimized: true,
   },
